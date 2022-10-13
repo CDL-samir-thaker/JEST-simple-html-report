6667b5cc49d0820b86b6e126d3109b37d2a457ea
Samir Thaker <samir.thaker@compassdigital.io>
2022-10-13 14:49:50 -0400

    first commit

---
+++ subtract.test.js
@@ -0,0 +1,8 @@
+const sub = require('./subtract');
+
+test('subtract 2 - 1 to equal 1', () => {
+  expect(sub(2, 1)).toBe(1);
+});
+test('subtract 5 - 1 to equal 4', () => {
+  expect(sub(5, 1)).toBe(4);
+});
